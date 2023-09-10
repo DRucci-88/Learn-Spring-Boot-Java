c0f11209545f5dece59a102c483dd4e5c818a63e
DRucci-88 <d.rucci.2001@gmail.com>
2023-09-11 02:17:09 +0700

    05 Dao and Service Initial

---
+++ 05-rest-crud-employee/src/main/java/com/lerucco/cruddemo/rest/EmployeeRestController.java
@@ -0,0 +1,26 @@
+package com.lerucco.cruddemo.rest;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lerucco.cruddemo.entity.Employee;
+import com.lerucco.cruddemo.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+}
