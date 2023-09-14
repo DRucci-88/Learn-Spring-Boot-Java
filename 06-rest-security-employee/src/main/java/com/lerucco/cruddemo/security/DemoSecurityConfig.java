d4e0e4c4664aad21da6c1feff03b134a1f61a293
DRucci-88 <d.rucci.2001@gmail.com>
2023-09-14 22:32:42 +0700

    06 JDBC Authentication plaintext and bcypt

--- 06-rest-security-employee/src/main/java/com/lerucco/cruddemo/security/DemoSecurityConfig.java
+++ 06-rest-security-employee/src/main/java/com/lerucco/cruddemo/security/DemoSecurityConfig.java
@@ -1,61 +1,72 @@
 package com.lerucco.cruddemo.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+// import org.springframework.security.core.userdetails.User;
+// import org.springframework.security.core.userdetails.UserDetails;
+// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}john")
-                .roles("EMPLOYEE")
-                .build();
+	// Add support for JDBC ... no more hardcoded users
+	@Bean
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
+		// Tell Spring Security to use JDBC authencation with our data source (database)
+		return new JdbcUserDetailsManager(dataSource);
+	}
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}mary")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
-        UserDetails le = User.builder()
-                .username("le")
-                .password("{noop}le")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
+		// Authorization rules given endpoints based on role
+		httpSecurity.authorizeHttpRequests(configurer -> configurer
+				.requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+				.requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+				// .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+				.requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
 
-        return new InMemoryUserDetailsManager(john, mary, le);
-    }
+		// Use HTTP Basic Authentication
+		httpSecurity.httpBasic(Customizer.withDefaults());
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		// Disable CSRF
+		// In general, not required for stateless REST APIs that use :
+		// POST, PUT, DELETE, PATCH
+		httpSecurity.csrf(csrf -> csrf.disable());
+		return httpSecurity.build();
 
-        // Authorization rules given endpoints based on role
-        httpSecurity.authorizeHttpRequests(configurer -> configurer
-                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                // .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
+	}
 
-        // Use HTTP Basic Authentication
-        httpSecurity.httpBasic(Customizer.withDefaults());
+	// @Bean
+	// public InMemoryUserDetailsManager userDetailsManager() {
+	// UserDetails john = User.builder()
+	// .username("john")
+	// .password("{noop}john")
+	// .roles("EMPLOYEE")
+	// .build();
 
-        // Disable CSRF
-        // In general, not required for stateless REST APIs that use :
-        // POST, PUT, DELETE, PATCH
-        httpSecurity.csrf(csrf -> csrf.disable());
-        return httpSecurity.build();
+	// UserDetails mary = User.builder()
+	// .username("mary")
+	// .password("{noop}mary")
+	// .roles("EMPLOYEE", "MANAGER")
+	// .build();
 
-    }
+	// UserDetails le = User.builder()
+	// .username("le")
+	// .password("{noop}le")
+	// .roles("EMPLOYEE", "MANAGER", "ADMIN")
+	// .build();
+
+	// return new InMemoryUserDetailsManager(john, mary, le);
+	// }
 
 }
