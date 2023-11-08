75579a8b35f11e401a3f69e4491817609fbf2e56
DRucci-88 <d.rucci.2001@gmail.com>
2023-11-08 12:05:15 +0700

    08 Initial Commit

---
+++ 08-thymeleaf-mvc/src/main/java/com/lerucco/thymeleafmvc/ThymeleafMvcApplication.java
@@ -0,0 +1,13 @@
+package com.lerucco.thymeleafmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafMvcApplication.class, args);
+	}
+
+}
