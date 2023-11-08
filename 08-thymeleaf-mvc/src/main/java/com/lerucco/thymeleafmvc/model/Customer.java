89f600db81e9487c8cb503f96e92487b1b960ad4
DRucci-88 <d.rucci.2001@gmail.com>
2023-11-08 15:02:49 +0700

    08 Regex

--- 08-thymeleaf-mvc/src/main/java/com/lerucco/thymeleafmvc/model/Customer.java
+++ 08-thymeleaf-mvc/src/main/java/com/lerucco/thymeleafmvc/model/Customer.java
@@ -1,6 +1,9 @@
 package com.lerucco.thymeleafmvc.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +15,13 @@ public class Customer {
     @Size(min = 1, message = "is required")
     private String firstName;
     private String lastName;
+
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    private int freePasses;
+
+    @NotNull(message = "is required")
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 digits")
+    private String postalCode;
 
 }
