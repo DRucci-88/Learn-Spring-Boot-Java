4f2abf2e5f9b20dd77c300b41b35e580f95e6ebb
DRucci-88 <d.rucci.2001@gmail.com>
2023-11-09 09:04:00 +0700

    09 Initial Commit

---
+++ 09-thymeleaf-mvc-crud/src/main/java/com/lerucco/thymeleafmvccrud/ThymeleafMvcCrudApplication.java
@@ -0,0 +1,13 @@
+package com.lerucco.thymeleafmvccrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafMvcCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafMvcCrudApplication.class, args);
+	}
+
+}
---
+++ 09-thymeleaf-mvc-crud/src/test/java/com/lerucco/thymeleafmvccrud/ThymeleafMvcCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lerucco.thymeleafmvccrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafMvcCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
